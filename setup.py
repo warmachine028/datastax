a398c3b434b990a99fad1f4d12bd4f3cd087b968
warmachine028 <warmachine028@outlook.com>
2023-08-03 22:37:44 +0530

    add: added readme-contents in setup.py

--- setup.py
+++ setup.py
@@ -11,38 +11,53 @@ try:  # https://stackoverflow.com/questions/30700166/python-open-file-error
 
 except Exception as error:
     readme_contents = (
-        'This library which supports ADTs like LinkedLists and Trees and its '
-        'types. This instant library is solely written from scratch and '
-        'requires no additional libraries to be installed. It solves the '
+        'This library which supports ADTs like Arrays, LinkedLists and Trees '
+        'and its types. This instant library is solely written from scratch '
+        'and requires no additional libraries to be installed. It solves the '
         'purpose of writing programs for complex data structures from scratch,'
         ' visualizing ADTs and simplify  writing its inner architectures. This'
         ' Module Supports the following dataStructures:\n'
-        '1. Arrays:\n'
+        '1. Nodes\n'
+        '   a. Node\n'
+        '   b. Doubly Node\n'
+        '   c. Tree Node\n'
+        '   d. AVL Node\n'
+        '   e. Red-Black Node\n'
+        '   f. Splay Node\n'
+        '   g. Heap Node\n'
+        '   h. Segment Node\n'
+        '   i. Huffman Node\n'
+        '   j. Threaded Node\n'
+        '2. Arrays:\n'
         '   a. Queue\n'
         '   b. Stack\n\n'
         '   c. Priority Queue\n\n'
-        '2. Lists:\n'
-        '   a. Singly Linked List\n'
-        '   b. Doubly Linked List\n'
-        '   c. Circular Linked List\n'
-        '   d. Doubly Circular List\n'
+        '3. Lists:\n'
+        '   a. Singly-Linked List\n'
+        '   b. Doubly-Linked List\n'
+        '   c. Circular-Linked List\n'
+        '   d. Doubly-Circular List\n'
         '   e. Queue\n\n'
         '   f. LRU Cache\n\n'
-        '3. Trees:\n'
+        '4. Trees:\n'
         '   a. Binary Tree\n'
-        '   b. Binary Search Tree\n'
-        '   c. AVL Tree\n'
-        '   d. Heap Tree\n'
-        '   e. Min Heap Tree\n'
-        '   f. Expression Tree\n'
-        '   e. Threaded Binary Tree\n'
-        '   f. Segment Trees\n'
-        '       i. Sum Segment Tree\n'
-        '      ii. Min Segment Tree\n'
-        '   g. Huffman Tree\n'
-        '   h. Red Black Tree\n'
-        '   i. Fibonacci Tree\n'
-        '   j. Splay Tree\n\n'
+        '   b. Search Trees\n'
+        '        i. Binary-Search Tree\n'
+        '       ii. AVL Tree\n'
+        '      iii. Red-Black Tree\n'
+        '       iv. Splay Tree\n'
+        '   c. Heap Trees\n'
+        '        i. Max-Heap Tree\n'
+        '       ii. Min-Heap Tree\n'
+        '   d. Segment Trees\n'
+        '       i. Sum-Segment Tree\n'
+        '      ii. Min-Segment Tree\n'
+        '   f. Huffman Tree\n'
+        '   g. Fibonacci Tree\n'
+        '   h. Expression Tree\n'
+        '   g. Threaded-Binary Tree\n\n'
+        '5. Tables\n'
+        '   a. HuffmanTable'
     )
     sys.stderr.write(f"Warning: Could not open README.md due {error}\n")
 setup(
@@ -91,14 +106,18 @@ setup(
     packages=[
         'datastax',
         'datastax/Utils',
-        'datastax/Nodes',
-        'datastax/Nodes/AbstractNodes',
+        'datastax/Utils/Exceptions',
+        'datastax/Utils/Warnings',
         'datastax/Arrays',
         'datastax/Arrays/AbstractArrays',
+        'datastax/Nodes',
+        'datastax/Nodes/AbstractNodes',
         'datastax/Lists',
         'datastax/Lists/AbstractLists',
         'datastax/Trees',
         'datastax/Trees/AbstractTrees',
+        'datastax/Tables',
+        'datastax/Tables/AbstractTables',
     ],
     author_email='pritamkundu771@gmail.com',
 )
